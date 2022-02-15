3289d8b656b5dbaeaf6008c2f28ecceb64de3ff8
tboy2022 <98777121+tboy2022@users.noreply.github.com>
2022-02-15 23:26:51 +0000

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,31 @@
+<html DOCTYPE!>
+<html>
+<header>
+<link rel="stylesheet" type="text/css" href="home.css"> <!--for external styling-->
+<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+<ul>
+	<a class="nav" > <!-- logo -->
+		<div class="logo">
+		<img src="InsertImage" alt="logo" height="100" width="100">
+		</div>
+	</a>
+<!--nav-->
+  <li class="dropdown"> <!--dropdown content-->
+	<a class="dropbtn" href="">Home</a>
+    <div class="dropdown-content">
+		<a href="latest.php">Latest</a>
+		<a href="trending.php">Trending</a>
+		<a href="highestrated.php">Highest rated</a>
+		<a href="upcoming.php">Upcoming</a>
+		<a href="aboutus.php"> About Us</a>
+		
+	</div>
+	</li>
+</ul>
+</header>
+<body>
+<h1>About Us</h1>
+
+</body>
+</html>
+{\rtf1}
