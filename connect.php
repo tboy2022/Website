eaaf359bd7831d60d28bef6ac1f7a3e1821a01fe
tboy2022 <98777121+tboy2022@users.noreply.github.com>
2022-03-09 16:13:58 +0000

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+	$connection = mysqli_connect('localhost', 'root', '', 'cms');
+ 
+	if(!$connection){
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    	exit;
+	}
+?>
