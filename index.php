6ceb1df4e444d9a053310898d9848ee93de35ed3
samirsarwar <98819163+samirsarwar@users.noreply.github.com>
2022-03-11 12:26:59 +0000

    Add files via upload
    CSS not connecting

--- index.php
+++ index.php
@@ -4,27 +4,95 @@
 <link rel="stylesheet" type="text/css" href="home.css"> <!--for external styling-->
 <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 <ul>
-	<a class="nav" > <!-- logo -->
+	 <!-- logo -->\
 		<div class="logo">
 		<img src="InsertImage" alt="logo" height="100" width="100">
-		</div>
-	</a>
-<!--nav-->
-  <li class="dropdown"> <!--dropdown content-->
-	<a class="dropbtn">Home</a>
-    <div class="dropdown-content">
-		<a href="latest.php">Latest</a>
-		<a href="trending.php">Trending</a>
-		<a href="highestrated.php">Highest rated</a>
-		<a href="upcoming.php">Upcoming</a>
-		<a href="aboutus.php"> About Us</a>
-		
-	</div>
-	</li>
-</ul>
+<div class="header">
+  <h1>Title of Website</h1>
+</div>
+
+<div class="topnav">
+  <a class= "active" href="">Home</a>
+  <a href="#">Categories</a>
+  <a href="#">Trending Games</a>
+  <a href="#">Highest Rated Games</a>
+  <a href="#">Upcoming Games</a>
+  <a href="#">About Us</a>
+</div>
+
+
 </header>
 <body>
-<h1>Home</h1>
+
+<h1> Homepage</h1>
+
+<!-- LOGIN LINK , SIGN UP LINK -->
+<br>
+
+<center>
+<button class="button button2">Sign Up</button>
+<button class="button button2">Sign in</button>
+</center>
+
+<br>
+
+<!--Slideshow Of Top Games -->
+
+<div class="topgames">
+  <div class="gallery">
+    <a target="_blank" href="INSERT IMG LINK">
+      <img src="INSERT IMG" alt="INSERT GAME NAME" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+
+<div class="topgames">
+  <div class="gallery">
+    <a target="_blank" href="INSERT IMG LINK">
+      <img src="INSERT IMG" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="topgames">
+  <div class="gallery">
+    <a target="_blank" href="INSERT IMG LINK">
+      <img src="INSERT IMG" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="topgames">
+  <div class="gallery">
+    <a target="_blank" href="INSERT IMG LINK">
+      <img src="INSERT IMG" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="clearfix"></div>
+<br> 
+
+<!-- Article Section -->
+<br>
+
+</br>
+<!-- CREDITS --> 
+<div class="footer">
+  <p>Footer</p>
+  <p> type here </p>
+</div>
+<div>
+<h2>
+
+
+</div>
+<br>
 
 </body>
 </html>
